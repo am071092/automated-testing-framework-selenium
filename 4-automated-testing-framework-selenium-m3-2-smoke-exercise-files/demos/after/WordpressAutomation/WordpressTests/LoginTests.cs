e429f10b86fb3f96cbd638c4defaca7878af6741
ankurMalhotra <ankur.malhotra7@ymail.com>
2016-04-28 11:49:21 -0700

    commit

---
+++ 4-automated-testing-framework-selenium-m3-2-smoke-exercise-files/demos/after/WordpressAutomation/WordpressTests/LoginTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordpressAutomation;
+
+namespace WordpressTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("admin").WithPassword("password").Login();
+
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login.");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Close();
+        }
+    }
+}
